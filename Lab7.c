4e466ba5b102c71a775f4e2e3762f060425bbfd3
Voiselle <125420821+Voiselle@users.noreply.github.com>
2023-03-22 23:17:41 -0400

    Update Lab7.c

--- Lab7.c
+++ Lab7.c
@@ -12,19 +12,21 @@ int main() {
     
     // Bubble Sort
     for (int i = 0; i < n - 1; i++) {
+        int pass_swaps = 0;  // Counter for swaps during current pass
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
-                swaps[j]++;
+                pass_swaps++;
             }
         }
+        swaps[i] = pass_swaps;
     }
     
-    // Print swaps for each index
-    for (int i = 0; i < n; i++) {
-        printf("Index %d: %d swaps\n", i, swaps[i]);
+    // Print swaps for each pass
+    for (int i = 0; i < n - 1; i++) {
+        printf("pass #%d: %d\n", i + 1, swaps[i]);
     }
     
     return 0;
